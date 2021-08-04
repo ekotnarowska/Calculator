3b5f6b8072609f65ab4155d845cb099623bccf01
agnik <technical.lotus@gmail.com>
2021-08-04 21:22:15 +0200

    Add tests for getDisplayNumber

---
+++ __tests__/getDisplayNumber.test.js
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const getDisplayNumber = require('.././www/js/getDisplayNumber');
+
+describe('getDisplayNumber', () => {
+
+    test('it creates a string from float number', () => {
+       const number = 33.55;
+       expect(getDisplayNumber(33.55)).toEqual('33.55');
+    });
+
+    test('it creates a string from integer', () => {
+        const number = 33;
+        expect(getDisplayNumber(33)).toEqual('33');
+     });
+ });
